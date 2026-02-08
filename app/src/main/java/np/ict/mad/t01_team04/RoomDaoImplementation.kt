22761bf8443923a41abe2f16e80fe9d94d3b451f
Sarrinah <nursarrinah@gmail.com>
2026-02-09 02:35:30 +0800

    feat(room): added watched entity and DAO to AppDatabase

--- app/src/main/java/np/ict/mad/t01_team04/RoomDaoImplementation.kt
+++ app/src/main/java/np/ict/mad/t01_team04/RoomDaoImplementation.kt
@@ -3,8 +3,9 @@ package np.ict.mad.t01_team04
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [ContentEntity::class, CommentEntity::class], version = 3)
+@Database(entities = [ContentEntity::class, CommentEntity::class, WatchedEntity::class], version = 4, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun contentDao(): ContentDao
     abstract fun commentDao(): CommentDao
+    abstract fun watchedDao(): WatchedDao
 }
